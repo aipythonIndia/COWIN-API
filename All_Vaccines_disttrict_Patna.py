ad49606b219046f7b0779c3a1344d7df6df8fedc
aipython <aipython.india@gmail.com>
2021-06-04 21:34:26 +0530

    Update All_Vaccines_disttrict_Patna.py

--- All_Vaccines_disttrict_Patna.py
+++ All_Vaccines_disttrict_Patna.py
@@ -18,17 +18,20 @@ Dist_ID = "097" # For Patna (Bihar)
 resp_JSON = {"sessions":[{"center_id":661924,"name":"GGS Patna City (Covaxin)","address":"Ashok Rajpath","state_name":"Bihar","district_name":"Patna","block_name":"Patna Sadar","pincode":800008,"from":"09:00:00","to":"18:00:00","lat":25,"long":85,"fee_type":"Free","session_id":"44551190-6c30-4d94-9d04-9ff73e3cecce","date":"05-06-2021","available_capacity_dose1":0,"available_capacity_dose2":0,"available_capacity":0,"fee":"0","min_age_limit":45,"vaccine":"COVAXIN","slots":["09:00AM-11:00AM","11:00AM-01:00PM","01:00PM-03:00PM","03:00PM-06:00PM"]}]}
 
 # Variables to store intermediate values
-time_interval = 30 # (in seconds) Specify the frequency of code execution
+time_interval = 20 # (in seconds) Specify the frequency of code execution
 vaccine_found_status_45 = False
 vaccine_found_status_18 = False
 sent_dates_record  = [] # List of dates for age group 45+ for which one round of message has been sent
-pincodes_served_till_now_45 = []
-pincodes_served_till_now_18 = []
+pincodes_served_today_45 = []
+pincodes_served_today_18 = []
+centers_served_today_45 = []
+centers_served_today_18 = []
 
 header = {'User-Agent': 'Chrome/84.0.4147.105 Safari/537.36'}
 
 def update_date_time():
-    print ("Updating date and Time ..")
+    print ()
+    print ("FUNCTION CALL: Updating date and Time ..")
     IST = pytz.timezone('Asia/Kolkata')
     raw_TS = datetime.now(IST) + timedelta(days=1) # One day ahead
     tomorrow_date = raw_TS.strftime("%d-%m-%Y")
@@ -37,18 +40,18 @@ def update_date_time():
     return today_date, tomorrow_date, curr_time
 
 def refresh_api_call(Dist_ID, tomorrow_date):
-    print ("Refreshing API call with updated parameters ...")
+    print ("FUNCTION CALL: Refreshing API call with updated parameters ...")
     request_link = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={Dist_ID}&date={tomorrow_date}"
     # print (request_link)
     response = requests.get(request_link, headers = header)
     resp_JSON = response.json()
-    # print ("Printing response: \n", resp_JSON)
+    print ("Size of Response in MB: ", len(response.content)/1000000)
+    print ("Response Header: ", response.headers)
     return resp_JSON
 
 
-
 def send_msg_on_telegram(msg, today_date):
-    print ("Sending Messege on Telegram ....")
+    print ("FUNCTION CALL: Sending Messege on Telegram ....")
     telegram_api_url = f"https://api.telegram.org/bot{tele_auth_token}/sendMessage?chat_id=@{tel_group_id}&text={msg}"
     
     tel_response = requests.get(telegram_api_url)
@@ -63,13 +66,14 @@ def get_availability_for_45(age_limit, available_capacity):
     # print ("Checking availability for Age 45 and above")
     if (age_limit == 45) and (available_capacity > 0):
         vaccine_found_status_45 = True
-        msg = f"""Vaccine AVAILABLE\n[For age 45+] at Pincode: {pincode}\nCenter: {center_name} on {slot_date}\nVaccine: {vaccine_name}\nDose_1: {qnty_dose_1}\nDose_2: {qnty_dose_2}\n\nRegistration link: https://selfregistration.cowin.gov.in/"""
-        if (pincode not in pincodes_served_till_now_45) or (str(today_date) not in sent_dates_record):
-            pincodes_served_till_now_45.append(pincode)
-            send_msg_on_telegram(msg, today_date)
-            # print (msg) # For local testing
+        msg = f"""Vaccine AVAILABLE\n[For age 45 and above] at Pincode: {pincode}\nCenter: {center_name} on {slot_date}\nVaccine: {vaccine_name}\nDose_1: {qnty_dose_1}\nDose_2: {qnty_dose_2}\n\nRegistration link: https://selfregistration.cowin.gov.in/"""
+        if (pincode not in pincodes_served_today_45) or (center_name not in centers_served_today_45) or (str(today_date) not in sent_dates_record):
+            pincodes_served_today_45.append(pincode)
+            centers_served_today_45.append(center_name)
+            # send_msg_on_telegram(msg, today_date)
+            print (msg) # For local testing
         else:
-            print (f"Corresponding msg for Age 45+ for Pincode {pincode} has already been served on {today_date}")
+            print (f"For Age 45+: Pincode {pincode} center name: {center_name} on {today_date} already served")
     else:
         vaccine_found_status_45 = False
         # print (f"Checked at [{today_date}-{curr_time}] : No Vaccine Slots for 45+ available for {tomorrow_date}")
@@ -79,13 +83,14 @@ def get_availability_for_18(age_limit, available_capacity):
     # print ("Checking availability for Age 18 to 44")
     if (age_limit == 18) and (available_capacity > 0):
         vaccine_found_status_18 = True
-        msg = f"""Vaccine AVAILABLE\n[For age 18+] at Pincode: {pincode}\nCenter: {center_name} on {slot_date}\nVaccine: {vaccine_name}\nDose_1: {qnty_dose_1}\nDose_2: {qnty_dose_2}\n\nRegistration link: https://selfregistration.cowin.gov.in/"""
-        if (pincode not in pincodes_served_till_now_18) or (str(today_date) not in sent_dates_record):        
-            pincodes_served_till_now_18.append(pincode)
-            send_msg_on_telegram(msg,today_date)
+        msg = f"""Vaccine AVAILABLE\n[For age 18 to 44] at Pincode: {pincode}\nCenter: {center_name} on {slot_date}\nVaccine: {vaccine_name}\nDose_1: {qnty_dose_1}\nDose_2: {qnty_dose_2}\n\nRegistration link: https://selfregistration.cowin.gov.in/"""
+        if (pincode not in pincodes_served_today_18) or (center_name not in centers_served_today_18) or (str(today_date) not in sent_dates_record):        
+            pincodes_served_today_18.append(pincode)
+            centers_served_today_18.append(center_name)
+            # send_msg_on_telegram(msg,today_date)
             print (msg) # For local testing
         else:
-            print (f"Corresponding msg for Age 18+ for Pincode {pincode} has already been served on {today_date}")
+            print (f"For Age 18+: Pincode {pincode} center name: {center_name} on {today_date} already served")
     else:
         vaccine_found_status_18 = False
         # print (f"Checked at [{today_date}-{curr_time}] : No Vaccine Slots for 18+ available for {tomorrow_date}")
@@ -128,6 +133,7 @@ while True:
             sent_dates_record.append(str(today_date))
 
         print (f"waiting........ for {time_interval} seconds")
+        print ()
         time.sleep(time_interval)
         
     else:
